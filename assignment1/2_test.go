ae372e17adaa95f263cbd7d43588ebfaaa12a72f
samkit <samkit993@gmail.com>
2016-01-28 18:45:32 +0530

    Minor changes

--- assignment1/2_test.go
+++ assignment1/2_test.go
@@ -8,7 +8,11 @@ import (
 	"strings"
 	"testing"
 )
-
+func checkError(t *testing.T, err error){
+    if err != nil {
+        t.Error(fmt.Sprintf("Error occurred: %s", err.Error())) // t.Error is visible when running `go test -verbose`
+    }
+}
 
 func TestTCPExtended(t *testing.T) {
 	name := "hello.txt"
@@ -19,13 +23,13 @@ func TestTCPExtended(t *testing.T) {
 		t.Error(err.Error()) 
 	}
 
-	scanner := bufio.NewScanner(conn)
+    reader := bufio.NewReader(conn)
 
 	// Write Test
 	fmt.Fprintf(conn, "write %v %v %v\r\n%v\r\n", name, len(contents), exptime, contents)
-	scanner.Scan() 
-	resp := scanner.Text() 
-	arr := strings.Split(resp, " ") 
+    resp, ok := reader.ReadBytes('\n')
+    checkError(t,ok)
+	arr := strings.Fields(string(resp)) 
 	expect(t, arr[0], "OK")
 	version, err := strconv.ParseInt(arr[1], 10, 64) 
 	if err != nil {
@@ -34,20 +38,30 @@ func TestTCPExtended(t *testing.T) {
 
 	//Read Test
 	fmt.Fprintf(conn, "read %v\r\n", name) 
-	scanner.Scan()
-	arr = strings.Split(scanner.Text(), " ")
+    resp, ok = reader.ReadBytes('\n')
+    checkError(t,ok)
+	arr = strings.Fields(string(resp))
 	expect(t, arr[0], "CONTENTS")
 	expect(t, arr[1], fmt.Sprintf("%v", version)) 
 	expect(t, arr[2], fmt.Sprintf("%v", len(contents)))	
-	scanner.Scan()
-	expect(t, contents, scanner.Text())
+
+    var contentRcvd = make([]byte,len(contents)+2)
+    n, ok := reader.Read(contentRcvd)
+    checkError(t,ok)
+    if n != len(contents)+2{
+        t.Error(t, fmt.Sprintf("%v",len(contents)+2), fmt.Sprintf("%v", n))
+    }
+    contentRcvdStr := string(contentRcvd)
+    clen := len(contentRcvdStr)
+    contentRcvdStr = contentRcvdStr[0:clen-2]
+	expect(t, contents, contentRcvdStr)
 
 	//CAS Case 1 Test
 	new_contents := "hello stupid world"
 	fmt.Fprintf(conn, "cas %v %v %v %v\r\n%v\r\n", name, version,len(new_contents), exptime, new_contents)
-	scanner.Scan()
-	resp = scanner.Text() 
-	arr = strings.Split(resp, " ") 
+    resp, ok = reader.ReadBytes('\n')
+    checkError(t,ok)
+	arr = strings.Fields(string(resp)) 
 	expect(t, arr[0], "OK")
 	version, err = strconv.ParseInt(arr[1], 10, 64) 
 	if err != nil {
@@ -56,24 +70,24 @@ func TestTCPExtended(t *testing.T) {
 
 	//CAS Case 2 Test
 	fmt.Fprintf(conn, "cas %v %v %v %v\r\n%v\r\n", name, version-1,len(new_contents), exptime, new_contents)
-	scanner.Scan()
-	resp = scanner.Text() 
-	arr = strings.Split(resp, " ") 
+    resp, ok = reader.ReadBytes('\n')
+    checkError(t,ok)
+	arr = strings.Fields(string(resp)) 
 	expect(t, arr[0], "ERR_VERSION")
 	expect(t, arr[1], fmt.Sprintf("%v",version))
 
 	//Delete Test
 	fmt.Fprintf(conn, "delete %v\r\n", name)
-	scanner.Scan()
-	resp = scanner.Text()
-	arr = strings.Split(resp, " ")
+    resp, ok = reader.ReadBytes('\n')
+    checkError(t,ok)
+	arr = strings.Fields(string(resp))
 	expect(t, arr[0], "OK")
 
 	//Read Fail Test
 	fmt.Fprintf(conn, "read %v\r\n", name)
-	scanner.Scan()
-	resp = scanner.Text()
-	arr = strings.Split(resp, " ")
+    resp, ok = reader.ReadBytes('\n')
+    checkError(t,ok)
+	arr = strings.Fields(string(resp))
 	expect(t, arr[0], "ERR_FILE_NOT_FOUND")
 }
 
